ef42a6e615829a02b1a4eb2c96794d4d4969f54c
devops0014 <106308110+devops0014@users.noreply.github.com>
2022-12-04 16:38:25 +0530

    credits udated

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIA4QUKASHDUCIVCMXL"
-secret_key = "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"
+access_key = "AKIA4QUKASHDTYIZ2HNS"
+secret_key = "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"
 }
