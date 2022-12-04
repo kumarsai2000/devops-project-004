619fc63825f5678da4bb8e993e323e318f4828d3
root <root@ip-172-31-40-116.ap-south-1.compute.internal>
2022-12-04 10:58:54 +0000

    terraform code

---
+++ Terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIA4QUKASHDUCIVCMXL"
+secret_key = "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"
+}
---
+++ Terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+default = "ami-074dc0a6f6c764218"
+}
+
+variable "instance_type" {
+default = "t2.micro"
+}
+
+variable "key_name" {
+default = "mainkeypair"
+}
+
+variable "name" {
+default = ""
+}
