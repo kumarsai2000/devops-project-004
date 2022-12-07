6694c41be693403faf13e914c27d125901025d4b
devops0014 <106308110+devops0014@users.noreply.github.com>
2022-12-07 14:45:48 +0530

    user updated

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-west-2"
-access_key = "AKIAW4OJVFZXZDXFUOVH"
-secret_key = "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"
+access_key = "AKIAW4OJVFZXS3VZKMHE"
+secret_key = "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"
 }
