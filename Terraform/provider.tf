7b465ec314d0166474f74971c9c5a5f089e43796
devops0014 <106308110+devops0014@users.noreply.github.com>
2022-12-07 22:28:43 +0530

    11111

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIA4QUKASHDXFMWWVB3"
-secret_key = "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"
+access_key = "AKIA4QUKASHDWUEXF3BY"
+secret_key = "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"
 }
