bc01799962ab85c5fe29f067faff32a70b6840cb
kumarsai2000 <116554600+kumarsai2000@users.noreply.github.com>
2022-12-11 11:42:09 +0530

    code cahnged

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "ap-east-1"
-access_key = "AKIA4QUKASHDQLONCLA3"
-secret_key = "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"
+access_key = "AKIAW4OJVFZXX6UEN2HQ"
+secret_key = "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"
 }
