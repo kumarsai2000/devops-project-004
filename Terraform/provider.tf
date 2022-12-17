95010ecb67429b2b7044e72aec8fd7bfbb3fe5d8
kumarsai2000 <116554600+kumarsai2000@users.noreply.github.com>
2022-12-17 21:06:27 +0530

    99

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAW4OJVFZXX6UEN2HQ"
-secret_key = "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"
+access_key = "AKIAW4OJVFZXX3B6OS6L"
+secret_key = "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"
 }
